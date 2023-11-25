3118ab443406d0524f6519397332c445e8f69de6
HamSlice7 <jhambly@uoguelph.ca>
2023-11-25 09:19:22 -0500

    re-adding my analysis steps

--- plink_analysis_rscript.R
+++ plink_analysis_rscript.R
@@ -1,3 +1,8 @@
-library("ggplot2")
+library("qqman")
 
 association_results_plink <- read.table("binary_run.qassoc", header = TRUE)
+
+manhattan(association_results_plink, col = c("red", "orange", "green", "blue", "purple"), main = "PLINK Manhattan Plot", suggestiveline = -log10(1e-7))
+
+qq(association_results_plink$P)
+
