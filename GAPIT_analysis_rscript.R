16817deb0fd7b449c7a3541e12902237f5aa0bc7
HamSlice7 <jhambly@uoguelph.ca>
2023-11-27 19:06:52 -0500

    Doing more GAPIT analysis

--- GAPIT_analysis_rscript.R
+++ GAPIT_analysis_rscript.R
@@ -6,11 +6,50 @@ source("http://zzlab.net/GAPIT/gapit_functions.txt")
 myY <- read.table("FT10_pheno_GAPIT.txt", head = TRUE)
 myG <- read.delim(file="genotype_vcf.hmp.txt",head= FALSE)
 
-#GWAS MLM
+#GWAS Run1 MLM
 myGAPIT_1=GAPIT(
   Y=myY, 
   G=myG,
   Model.selection = TRUE
   )
 
+#GWAS run 2
+GAPIT_2 <- GAPIT(
+  Y=myY, 
+  G=myG,
+  Model.selection = TRUE, 
+  model = c("MLM")
+)
 
+
+#GWAS run 3, MLM PCA=3
+GAPIT_3 <- GAPIT(
+  Y=myY, 
+  G=myG,
+  PCA.total=3,
+)
+
+#GWAS run 4 BLINK PC =?, 
+GAPIT_4 <- GAPIT(
+  Y=myY, 
+  G=myG,
+  Model.selection = TRUE,
+  model = c("BLINK")
+)
+
+#GWAS run 5 BLINK PC =3, 
+GAPIT_5 <- GAPIT(
+  Y=myY, 
+  G=myG,
+  PCA.total=3,
+  model = c("BLINK")
+)
+
+
+#GWAS run 6 GLM 
+GAPIT_6 <- GAPIT(
+  Y=myY, 
+  G=myG,
+  Model.selection = TRUE,
+  model = c("GLM")
+)
--- plink_analysis_rscript.R
+++ plink_analysis_rscript.R
@@ -1,8 +1,23 @@
 library("qqman")
+library("tidyverse")
 
 association_results_plink <- read.table("binary_run.qassoc", header = TRUE)
 
-manhattan(association_results_plink, col = c("red", "orange", "green", "blue", "purple"), main = "PLINK Manhattan Plot", suggestiveline = -log10(1e-7))
+manhattan(association_results_plink, col = c("red", "orange", "green", "blue", "purple"), main = "PLINK Manhattan Plot Unadjusted", suggestiveline = -log10(1e-7))
 
 qq(association_results_plink$P)
 
+
+adjusted_assocaition_results_plink <- read.table("binary_run.qassoc.adjusted_withGC.txt", header = TRUE)
+
+GC_adjusted_assocaition_results_plink <- adjusted_assocaition_results_plink |>
+  select(CHR, SNP, GC, BP)
+
+#change column name GC to P
+colnames(GC_adjusted_assocaition_results_plink) <- c("CHR", "SNP", "P", "BP")
+
+
+#need BP
+manhattan(GC_adjusted_assocaition_results_plink, col = c("red", "orange", "green", "blue", "purple"), main = "PLINK Manhattan Plot GC Adjusted", suggestiveline = -log10(1e-5))
+
+qq(GC_adjusted_assocaition_results_plink$GC)
